7b64ab95558fd5351ab4f2fa758b6ae74e25b259
harryfung22 <harryfung22@gmail.com>
2024-01-02 20:54:14 -0500

    initialize repo

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Connection to MongoDB successful. Listening on port: ${port}`)
+})
