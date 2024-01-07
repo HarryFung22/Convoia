411130450afff9bc7bcc52de686601d4713b114f
harryfung22 <harryfung22@gmail.com>
2024-01-07 10:52:38 -0500

    web socket implemented

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(uri).then(() => {
-    app.listen(port, () => console.log(`MongoDB Connection Established. Listening on port: ${port}`))
+    console.log(`MongoDB Connection Established. Listening on port: ${port}`)
 }).catch((error) => {console.log(error)})
 
 app.use(userAPI, userRoutes);
@@ -29,3 +29,50 @@ app.use(messageAPI, messageRoutes);
 // error handling
 app.use(notFound);
 app.use(errorHandler);
+
+
+const server = app.listen(port, console.log(`Server connection established. Listening on port: ${port}`));
+const io = require('socket.io')(server, {
+    //inactivity, close connection
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("Socket Connection established")
+
+    socket.on('setup', (user) => {
+        socket.join(user._id);
+        socket.emit('connected')
+    })
+
+    //setup room with chat id
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+    
+    socket.on("new message", (newMessageRecieved) => {
+        //chatID
+        var chat = newMessageRecieved.chat;
+    
+        if (!chat.users) return console.log("chat.users not defined");
+    
+        chat.users.forEach((user) => {
+        //if sent by current user, return
+        if (user._id == newMessageRecieved.sender._id) return;
+    
+        //inside that users room, emits/sends msg
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+        });
+    });
+    
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    });
+})
